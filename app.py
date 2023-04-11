b5e934e6e1b7657d6b031ab15c23c890befec3ec
kanishkalmdmax <130255489+kanishkalmdmax@users.noreply.github.com>
2023-04-11 17:06:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def download(filename):
     return send_file(filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000),debug=True)
