6216cc0699ec5c750c27780e9a01af5374c30241
kanishkalmdmax <130255489+kanishkalmdmax@users.noreply.github.com>
2023-04-16 07:21:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, send_file
-import openpyxl
-from openpyxl.styles import Alignment, Border, Side, PatternFill
+from werkzeug.utils import secure_filename
+import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment, PatternFill, Border, Side
 import os
 
 app = Flask(__name__)
@@ -8,10 +10,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '''
-        <html>
-            <head>
-                <title>Netradyne E-Mail</title>
-                <style>
+    <!doctype html>
+    <html>
+        <head>
+            <title>Netradyne E-Mail Daily</title>
+            <style>
                     #instructions {
                         border: 1px solid black;
                         padding: 10px;
@@ -20,101 +23,147 @@ def index():
                         right: 10px;
                         transform: translateY(-50%);
                     }
-                </style>
-            </head>
-            <body>
-                <form action="/upload" method="post" enctype="multipart/form-data">
-                    <input type="file" name="file">
-                    <input type="submit" value="Upload">
-                </form>
+            </style>
+        </head>
+        <body>
+            <h3>Netradyne E-Mail Grid (Daily)</h3>
+            <form action="/upload" method="post" enctype="multipart/form-data">
+                <input type="file" name="file" accept=".xlsx"><br><br>
+                <input type="submit" value="Upload">
+            </form>
                 <div id="instructions">
-                    Steps:<br>(Upload Reports in the Performance App first)<br>1. Open Performance App, then select Reports and then Driver Report.<br>2. Click on the Daily button and select yesterday's date, then toggle the Summary option and click Download.<br>3. Once the file is downloaded, open this URL, click on Choose File, select the file, and click Upload.<br>4. Then a new page will open with the Download button, click on that and download the file.<br>5. Open the file, it'll be the same file, with a new sheet added with the name of "Extracted Data", open that sheet.<br>6. This sheet will have the email grid, you can copy this grid and paste it in the email.
+                    Steps:<br>(Upload Reports in the Performance App first)<br>1. Open Performance App, then select Reports and then Driver Report.<br>2. Click on the Daily button and select yesterday's date, then toggle the Summary option and click Download.<br>3. Once the file is downloaded, open this URL, click on Choose File, select the file, and click Upload.<br>4. Then a new page will open with the Download button, click on that and download the file.<br>5. This file will have the email grid, you can copy this grid and paste it in the email.
                 </div>
-            </body>
-        </html>
+        </body>
+    </html>
     '''
 
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
-    filename = file.filename
+    filename = secure_filename(file.filename)
+
+    # Check if the uploaded file is an excel file
+    if not filename.endswith('.xlsx'):
+        return '''
+        <!doctype html>
+        <html>
+            <head>
+                <title>Netradyne E-Mail Daily</title>
+            </head>
+            <body>
+                <h3>Error: This is not a XLSX file</h3>
+                <p>This is not a XLSX file, please download &amp; upload the file from Performance App&gt;Driver Report&gt;Summary</p>
+                <a href="/">Return to upload form</a>
+            </body]
+        </html>
+        '''
+
     file.save(filename)
 
-    # Open the Excel file and select the appropriate sheet
-    wb = openpyxl.load_workbook(filename)
-    sheet = wb['Sheet1']
-
-    # Create a new sheet to hold the extracted data
-    new_sheet = wb.create_sheet('Extracted Data')
-
-    # Initialize the row and column counters for the new sheet
-    new_row = 1
-    new_col = 1
-
-    # Define border style
-    thin_border = Border(left=Side(style='thin'), right=Side(style='thin'), top=Side(style='thin'), bottom=Side(style='thin'))
-
-    # Define header fill
-    header_fill = PatternFill(start_color='B8CCE4', end_color='B8CCE4', fill_type='solid')
-
-    # Write the headers for the new sheet and apply formatting
-    new_sheet.cell(row=new_row, column=new_col, value='Name').fill = header_fill
-    new_sheet.cell(row=new_row, column=new_col+1, value='Violations').fill = header_fill
-    new_sheet.cell(row=new_row, column=new_col+2, value='Violations Count').fill = header_fill
-
-    # Loop through each row in the original sheet
-    for row in range(2, 101):
-        # Check if any of the violation columns have a value greater than 0
-        has_violation = False
-        violation_types = []
-        for col in range(3, 11):
-            cell_value = sheet.cell(row=row, column=col).value
-            if cell_value is not None and cell_value > 0:
-                has_violation = True
-                violation_types.append(sheet.cell(row=1, column=col).value)
-
-        # If a violation was found, write the name and violation types to the new sheet
-        if has_violation:
-            new_row += 1
-            new_sheet.cell(row=new_row, column=new_col, value=sheet.cell(row=row, column=1).value).border = thin_border
-            new_sheet.cell(row=new_row, column=new_col+1, value=', '.join(violation_types)).border = thin_border
-
-        # Sum the violation values and write the total to the new sheet
-        violation_sum = sum(sheet.cell(row=row, column=col).value for col in range(3, 11) if sheet.cell(row=row, column=col).value is not None)
-        if violation_sum > 0:
-            new_sheet.cell(row=new_row, column=new_col+2, value=violation_sum).border = thin_border
-
-    # Apply formatting to all cells in new sheet
-    for col in new_sheet.columns:
-        max_length = 0
-        column = col[0].column_letter
-        for cell in col:
-            try:
-                if len(str(cell.value)) > max_length:
-                    max_length = len(cell.value)
-            except:
-                pass
-        adjusted_width = (max_length + 2)
-        new_sheet.column_dimensions[column].width = adjusted_width
-        for cell in col:
-            cell.alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
-            cell.border = thin_border
-
-    # Save the updated Excel file
-    wb.save(filename)
-
-    return '''
+    # Load the excel file
+    try:
+        df = pd.read_excel(filename)
+    except Exception as e:
+        return '''
+        <!doctype html>
         <html>
-        <title>Netradyne E-Mail</title>
+            <head>
+                <title>Netradyne E-Mail Daily</title>
+            </head>
             <body>
-                <a href="/download/{}">Download</a>
+                <h3>Error: This is not a valid Driver Report file</h3>
+                <p>This is not a valid Driver Report file, please download the correct file, please download &amp; upload the file from Performance App&gt;Driver Report&gt;Summary</p>
+                <a href="/">Return to upload form</a>
             </body>
         </html>
-    '''.format(filename)
+        '''
 
-@app.route('/download/<filename>')
+    # Define the columns to extract data from
+    columns = ['Name', 'Following Distance', 'Camera Obstruction', 'U Turn', 'Driver Distraction', 'Seatbelt Compliance', 'Sign Violations', 'Speeding Violations', 'Traffic Light Violation']
+
+    # Check if the excel file has the required columns
+    if not all(column in df.columns for column in columns):
+        return '''
+        <!doctype html>
+        <html>
+            <head>
+                <title>Netradyne E-Mail Daily</title>
+            </head>
+            <body>
+                <h3>Error: This is not a valid Driver Report file</h3>
+                <p>This is not a valid Driver Report file, please download the correct file, please download &amp; upload the file from Performance App&gt;Driver Report&gt;Summary</p>
+                <a href="/">Return to upload form</a>
+            </body>
+        </html>
+        '''
+
+    # Filter the rows based on the condition
+    df = df[(df['Following Distance'] > 0) | (df['Camera Obstruction'] > 0) | (df['U Turn'] > 0) | (df['Driver Distraction'] > 0) | (df['Seatbelt Compliance'] > 0) | (df['Sign Violations'] > 0) | (df['Speeding Violations'] > 0) | (df['Traffic Light Violation'] > 0)]
+
+    # Create a new column for Violations
+    df['Violations'] = df[columns[1:]].apply(lambda x: ', '.join(x[x>0].index), axis=1)
+
+    # Create a new column for Violations Count
+    df['Violations Count'] = df[columns[1:]].apply(lambda x: x[x>0].sum(), axis=1)
+
+    # Select the required columns
+    df = df[['Name', 'Violations', 'Violations Count']]
+
+    # Write the output to a new excel file
+    output_file = filename.replace('.xlsx', '_grid.xlsx')
+    writer = pd.ExcelWriter(output_file, engine='openpyxl')
+    df.to_excel(writer, sheet_name='Extracted Data', index=False)
+
+    # Apply formatting to the cells
+    workbook = writer.book
+    worksheet = writer.sheets['Extracted Data']
+
+    # Set alignment to center and middle for all cells
+    for row in worksheet.iter_rows():
+        for cell in row:
+            cell.alignment = Alignment(horizontal='center', vertical='center')
+
+    # Set fill color for cells A1:C1
+    fill = PatternFill(fill_type='solid', fgColor='B8CCE4')
+    for cell in worksheet['A1:C1'][0]:
+        cell.fill = fill
+        
+    # Apply border to all cells
+    border = Border(left=Side(style='thin'), right=Side(style='thin'), top=Side(style='thin'), bottom=Side(style='thin'))
+    for row in worksheet.iter_rows():
+        for cell in row:
+            cell.border = border
+
+    # Adjust column width to fit data
+    for column in worksheet.columns:
+        max_length = 0
+        column_letter = column[0].column_letter
+        for cell in column:
+            max_length = max(max_length, len(str(cell.value)))
+        adjusted_width = (max_length + 2) * 1.1
+        worksheet.column_dimensions[column_letter].width = adjusted_width
+
+    writer.close()
+
+    # Return response with download button
+    return '''
+    <!doctype html>
+    <html>
+        <head>
+            <title>Netradyne E-Mail Daily</title>
+        </head>
+        <body>
+            <h3>Netradyne E-Mail Daily</h3>
+            <p>File processed successfully!</p>
+            <a href="/download/{}"><button>Download</button></a>
+        </body>
+    </html>
+    '''.format(output_file)
+
+@app.route('/download/<path:filename>')
 def download(filename):
-    return send_file(filename)
+    return send_file(filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000),debug=True)
